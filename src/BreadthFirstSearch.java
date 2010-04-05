0c9894349596e1740bca36279cba859ee7382a22
kersanske.b <kersanske.b@husky.neu.edu>
2010-04-05 20:31:49 +0000

    Added more content and the basic common abstract search algorithm in the Search class.

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,9 +1,13 @@
+import java.util.List;
 
-public class BreadthFirstSearch {
+
+public class BreadthFirstSearch extends Search{
 
 	private Node startingNode;
-	
-	
-	
-	
+
+	@Override
+	protected List<Node> retrieveResultingNodesToAddToFrontier(Node nodeToExpand, Problem problem) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 }
